0842dbb42788160092008a97d89f56dfc209c940
AndreiBrasso <andrei.brasso@gmail.com>
2016-11-18 03:12:00 +0100

    404 remove tone of stuff

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ let app = express();
 
 app.use(express.static('public'));
 
-app.listen(80, function () {
+app.use(function(req, res, next){
+    res.status(404).sendFile(__dirname + '/public/404.html');
+});
+
+app.listen(8080, function () {
     console.log('Started on port 80!')
 });
