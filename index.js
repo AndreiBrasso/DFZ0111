958eab6043914c9d59f405960af6697028552cba
AndreiBrasso <andrei.brasso@gmail.com>
2016-11-18 03:55:59 +0100

    Change port

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.use(function(req, res, next){
     res.status(404).sendFile(__dirname + '/public/404.html');
 });
 
-app.listen(8080, function () {
+app.listen(80, function () {
     console.log('Started on port 80!')
 });
